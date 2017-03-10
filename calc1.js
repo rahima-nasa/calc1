04de6a9675b973d7d7c9069cb333fc6debbe9eca
Rahimon Nasa <Rahima@Rahimons-MacBook-Air.local>
2017-03-10 00:49:30 -0500

    first commit

---
+++ calc1.js
@@ -0,0 +1,66 @@
+//first declare counters for total calories and total cost globally and then set to zero.
+var totalAdd = 0,
+	totalSubtract = 0;
+totalDivide = 0,
+	totalMultiply = 0;
+
+$(document).ready(function () { //on DOM ready, bind clicks
+	$("#compute").on("click", function (e) {
+		e.preventDefault(); //disable the button's default behavior
+
+		$(".numbah").each(function () {
+
+			var numbah = $(this),
+				x_value = parseInt('#x'.val()),
+				y_value = parseInt('#y'.val()),
+
+				//add the values
+				update_total_add(x_value, y_value);
+			//subtract the values
+			update_total_subtract(x_value, y_value);
+			//divide the values
+			update_total_divide(x_value, y_value);
+			//multiply the values
+			update_total_multiply(x_value, y_value);
+
+		}); //end each
+
+		// output the results
+		show_results();
+	});
+});
+
+//defining the functions outside of document.ready
+
+function update_total_add(x_value, y_value) {
+	//add the values
+	var total_add_for_numbahs = x_value + y_value;
+	totalAdd = totalAdd + total_add_for_numbahs;
+}
+
+function update_total_subtract(x_value, y_value) {
+	//subtract the values
+	var total_subtract_for_numbahs = x_value - y_value;
+	totalSubtract = totalSubtract + total_subtract_for_numbahs;
+}
+
+function update_total_multiply(x_value, y_value) {
+	//multiply the values
+	var total_multiply_for_numbahs = x_value * y_value;
+	totalMultiply = totalMultiply + total_multiply_for_numbahs;
+}
+
+function update_total_divide(x_value, y_value) {
+	//divide the values
+	var total_multiply_for_numbahs = x_value / y_value;
+	totalDivide = totalDivide + total_divide_for_numbahs;
+}
+
+
+function show_results() {
+	//totalCost and totalCalories are GLOBAL variables. This function can see them no problem!
+	$("#add").val(totalAdd);
+	$("#subtract").val(totalSubtract);
+	$("#divde").val(totalDivide);
+	$("#multiply").val(totalMultiply);
+}
